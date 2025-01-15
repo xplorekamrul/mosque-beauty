83049adfd4f566967c8aec9f614a779b272e6a53
mdkamruzzaman32 <mdkamruzzaman099826@gmail.com>
2025-01-15 19:24:26 +0600

    hero responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // Hero timer strat
 function updateTimer() {
   const now = new Date();
+
+  const date = String(now.getDate()).padStart(2, '0');
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
 
+  document.getElementById('date').textContent = date;
   document.getElementById('hours').textContent = hours;
   document.getElementById('minutes').textContent = minutes;
   document.getElementById('seconds').textContent = seconds;
