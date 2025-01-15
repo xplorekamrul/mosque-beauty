8b1b3ae7ede2aef70b9d3b6f861166596e2d1088
mdkamruzzaman32 <mdkamruzzaman099826@gmail.com>
2025-01-15 13:04:44 +0600

    date update

--- js/script.js
+++ js/script.js
@@ -1,103 +1,15 @@
-// autotype text effect start 
-var typed = new Typed(".txt-rotate", {
-    strings: ["Md.Kamruzzaman", "Rafid Hasan", "Niazul Islam", "Ariful Islam"],
-    typeSpeed: 150,
-    backSpeed:50,
-    looped:true
-  });
-// autotype text effect end
-//Counter up start
-$('.counter').counterUp({
-  delay: 10,
-  time: 2500
-});
+// Hero timer strat
+function updateTimer() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
 
+  document.getElementById('hours').textContent = hours;
+  document.getElementById('minutes').textContent = minutes;
+  document.getElementById('seconds').textContent = seconds;
+}
 
-
-//Counter up end
-// 3d text effect start 
-const Texts=[
-  'HTML', 'CSS', 'JAVASCRIPT',
-  'TELWIN CSS','NODE JS','REACT',
-  'PYTHON','JAVA', 'JQUERY','JAVA',
-  'C#','MY SQL', 'GITHUB','C++',
-  'Photoshop', 'Illustrator',"c",
-  'Figma'
-];
-var tagCloud = TagCloud('.sphere', Texts,{
-  //sphere radius in px
-  radius:170,
-
-  //animation speed 
-  maxSpeed:'normal',
-  initSpeed:'fast',
-
-  //rolling direction
-  direction:135,
-
-  //interaction with mouse 
-  keep:true,
-
-});
-
-  // sphere color 
-  var color = '#0deb32';
-  document.querySelector('.sphere').style.color = color;
-
-// 3d text effect end
-
-
-// mixitup code start 
-
-var containerEl = document.querySelector('.gallarycontent');
-
-var mixer = mixitup(containerEl);
-// mixitup code end
-
-
-// slick slider start 
-
-// console.log("hello world");
-// $('.slider').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-//     prevArrow:'<i class="fa-solid fa-chevron-left prev"></i>',
-//     nextArrow:'<i class="fa-solid fa-chevron-right next"></i>',
-//     dots:true,
-    
-//   });
-
-
-$('.slider').slick({
-  centerMode: true,
-  centerPadding: '200px',
-  slidesToShow: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  prevArrow:'<i class="fa-solid fa-chevron-left prev"></i>',
-  nextArrow:'<i class="fa-solid fa-chevron-right next"></i>',
-  dots:true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
-// slick slider end
+setInterval(updateTimer, 1000);
+updateTimer(); // Initial call to set the timer immediately
+// Hero timer end
